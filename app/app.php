980a8f0ffaf012bb34c5f7cb42f101d53c704497
Eliot Carlsen <eliot.carlsen@gmail.com>
2017-03-31 14:32:49 -0700

    add html pages with edit functionality, add edit function in stylist class page with passing test

--- app/app.php
+++ app/app.php
@@ -4,6 +4,9 @@
   require_once __DIR__."/../src/Stylist.php";
   require_once __DIR__."/../src/Client.php";
 
+  use Symfony\Component\HttpFoundation\Request;
+  Request::enableHttpMethodParameterOverride();
+
   use Symfony\Component\Debug\Debug;
   Debug::enable();
   $app = new Silex\Application();
@@ -28,6 +31,18 @@
     $new_stylist->save();
     $stylists = Stylist::getAll();
     return $app['twig']->render('index.html.twig', array ('stylists'=>$stylists));
+  });
+
+  $app -> get("/stylist/{id}/edit", function($id) use($app) {
+      $stylist = Stylist::find($id);
+      return $app['twig']->render('stylist_edit.html.twig', array('stylist'=>$stylist));
+  });
+
+  $app->patch("/stylist/{id}", function($id) use($app) {
+      $name = $_POST['name'];
+      $stylist = Stylist::find($id);
+      $stylist->update($name);
+      return $app['twig']->render('stylist.html.twig', array('stylist'=>$stylist));
   });
 
   $app -> get("/stylists/{id}", function($id) use($app) {
